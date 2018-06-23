8ea8aa87d7bb427279230cc824542cd61c458c81
Sunit Katkar <sunitkatkar@gmail.com>
2018-06-23 11:57:33 -0700

    Merging ArkaBando's changes using local repo as I made a mistake in merging pull requests on Github

--- src/main/java/com/sunitkatkar/blogspot/tenant/config/DataSourceBasedMultiTenantConnectionProviderImpl.java
+++ src/main/java/com/sunitkatkar/blogspot/tenant/config/DataSourceBasedMultiTenantConnectionProviderImpl.java
@@ -37,93 +37,91 @@ import com.sunitkatkar.blogspot.util.DataSourceUtil;
 import com.sunitkatkar.blogspot.util.TenantContextHolder;
 
 /**
- * This class does the job of selecting the correct database based on the tenant
- * id found by the {@link CurrentTenantIdentifierResolverImpl}
+ * This class does the job of selecting the correct database based on the tenant id found by the
+ * {@link CurrentTenantIdentifierResolverImpl}
  * 
- * @author Sunit Katkar, sunitkatkar@gmail.com
- *         (https://sunitkatkar.blogspot.com/)
+ * @author Sunit Katkar, sunitkatkar@gmail.com (https://sunitkatkar.blogspot.com/)
  * @since ver 1.0 (May 2018)
  * @version 1.0
  *
  */
 @Configuration
 public class DataSourceBasedMultiTenantConnectionProviderImpl
-		extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl {
+        extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl {
 
-	private static final Logger LOG = LoggerFactory.getLogger(DataSourceBasedMultiTenantConnectionProviderImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DataSourceBasedMultiTenantConnectionProviderImpl.class);
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Injected MasterTenantRepository to access the tenant information from the
-	 * master_tenant table
-	 */
-	@Autowired
-	private MasterTenantRepository masterTenantRepo;
+    /**
+     * Injected MasterTenantRepository to access the tenant information from the master_tenant table
+     */
+    @Autowired
+    private MasterTenantRepository masterTenantRepo;
 
-	/**
-	 * Map to store the tenant ids as key and the data source as the value
-	 */
-	private Map<String, DataSource> dataSourcesMtApp = new TreeMap<>();
+    /**
+     * Map to store the tenant ids as key and the data source as the value
+     */
+    private Map<String, DataSource> dataSourcesMtApp = new TreeMap<>();
 
-	@Override
-	protected DataSource selectAnyDataSource() {
-		// This method is called more than once. So check if the data source map
-		// is empty. If it is then rescan master_tenant table for all tenant
-		// entries.
-		if (dataSourcesMtApp.isEmpty()) {
-			List<MasterTenant> masterTenants = masterTenantRepo.findAll();
-			LOG.info(">>>> selectAnyDataSource() -- Total tenants:" + masterTenants.size());
-			for (MasterTenant masterTenant : masterTenants) {
-				dataSourcesMtApp.put(masterTenant.getTenantId(),
-						DataSourceUtil.createAndConfigureDataSource(masterTenant));
-			}
-		}
-		return this.dataSourcesMtApp.values().iterator().next();
-	}
+    @Override
+    protected DataSource selectAnyDataSource() {
+        // This method is called more than once. So check if the data source map
+        // is empty. If it is then rescan master_tenant table for all tenant
+        // entries.
+        if (dataSourcesMtApp.isEmpty()) {
+            List<MasterTenant> masterTenants = masterTenantRepo.findAll();
+            LOG.info(">>>> selectAnyDataSource() -- Total tenants:" + masterTenants.size());
+            for (MasterTenant masterTenant : masterTenants) {
+                dataSourcesMtApp.put(masterTenant.getTenantId(),
+                        DataSourceUtil.createAndConfigureDataSource(masterTenant));
+            }
+        }
+        return this.dataSourcesMtApp.values().iterator().next();
+    }
 
-	@Override
-	protected DataSource selectDataSource(String tenantIdentifier) {
-		// If the requested tenant id is not present check for it in the master
-		// database 'master_tenant' table
-		
-		tenantIdentifier = initializeTenantIfLost(tenantIdentifier);
-		
-		if (!this.dataSourcesMtApp.containsKey(tenantIdentifier)) {
-			List<MasterTenant> masterTenants = masterTenantRepo.findAll();
-			LOG.info(
-					">>>> selectDataSource() -- tenant:" + tenantIdentifier + " Total tenants:" + masterTenants.size());
-			for (MasterTenant masterTenant : masterTenants) {
-				dataSourcesMtApp.put(masterTenant.getTenantId(),
-						DataSourceUtil.createAndConfigureDataSource(masterTenant));
-			}
-		}
-		return this.dataSourcesMtApp.get(tenantIdentifier);
-	}
-	
-	/**
-	 * Initialize tenantId based on the logged in user if the tenant Id got lost in after form submission in a user session.
-	 * @param tenantIdentifier
-	 * @return tenantIdentifier
-	 */
-	private String initializeTenantIfLost(String tenantIdentifier) {
-//		if (TenantContextHolder.getTenant() == null) {
-//
-//			SecurityContext securityContext = SecurityContextHolder.getContext();
-//			Authentication authentication = securityContext.getAuthentication();
-//			CustomUserDetails customUserDetails = null;
-//			if (authentication != null) {
-//				Object principal = authentication.getPrincipal();
-//				customUserDetails = principal instanceof CustomUserDetails ? (CustomUserDetails) principal : null;
-//			}
-//			TenantContextHolder.setTenantId(customUserDetails.getTenant());
-//		}
+    @Override
+    protected DataSource selectDataSource(String tenantIdentifier) {
+        // If the requested tenant id is not present check for it in the master
+        // database 'master_tenant' table
 
-		 TenantContextHolder.setTenantId("tenant_1");
+        tenantIdentifier = initializeTenantIfLost(tenantIdentifier);
 
-		if (tenantIdentifier != TenantContextHolder.getTenant()) {
-			tenantIdentifier = TenantContextHolder.getTenant();
-		}
-		return tenantIdentifier;
-	}
+        if (!this.dataSourcesMtApp.containsKey(tenantIdentifier)) {
+            List<MasterTenant> masterTenants = masterTenantRepo.findAll();
+            LOG.info(
+                    ">>>> selectDataSource() -- tenant:" + tenantIdentifier + " Total tenants:" + masterTenants.size());
+            for (MasterTenant masterTenant : masterTenants) {
+                dataSourcesMtApp.put(masterTenant.getTenantId(),
+                        DataSourceUtil.createAndConfigureDataSource(masterTenant));
+            }
+        }
+        return this.dataSourcesMtApp.get(tenantIdentifier);
+    }
+
+    /**
+     * Initialize tenantId based on the logged in user if the tenant Id got lost in after form submission in a user
+     * session.
+     * 
+     * @param tenantIdentifier
+     * @return tenantIdentifier
+     */
+    private String initializeTenantIfLost(String tenantIdentifier) {
+        if (TenantContextHolder.getTenant() == null) {
+
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            Authentication authentication = securityContext.getAuthentication();
+            CustomUserDetails customUserDetails = null;
+            if (authentication != null) {
+                Object principal = authentication.getPrincipal();
+                customUserDetails = principal instanceof CustomUserDetails ? (CustomUserDetails) principal : null;
+            }
+            TenantContextHolder.setTenantId(customUserDetails.getTenant());
+        }
+
+        if (tenantIdentifier != TenantContextHolder.getTenant()) {
+            tenantIdentifier = TenantContextHolder.getTenant();
+        }
+        return tenantIdentifier;
+    }
 }
